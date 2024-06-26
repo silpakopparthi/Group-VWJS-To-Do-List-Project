61ccc65c5f34c1948c34c8a1e2a80e014efe8fdb
richard <hyacinthzy1@gmail.com>
2024-06-26 19:52:56 +1000

    add editing function and refactor todolist

--- to-do-list-project/src/components/ToDoPage.jsx
+++ to-do-list-project/src/components/ToDoPage.jsx
@@ -2,64 +2,110 @@ import { useState } from "react";
 import ToDoItem from "./ToDoItem";
 import ToDoList from "./ToDoList";
 //Parent component that hosts ToDoItem and ToDoList child components
-function ToDoPage(){
-    const [displayToDoItem, setDisplayToDoItem] = useState(false);
-    //Array of tasks 
-    const [listItems, setlistItems] = useState([]);
-    //Flag to hold if the add new item button has to be shown
-    const [showAddNewItemButton, setshowAddNewItemButton] = useState(true);
-    //Function called from the child object to add a new item to the litsitems array
-    const addItemtoList = (title, description, status,assignee,duedate)=>{
-        setDisplayToDoItem(false);
-        //Display the add button again when a new item is added
-        setshowAddNewItemButton(!showAddNewItemButton);
-        if(title!=""){
-            let itemObj = {
-                itemID:Math.floor(Math.random() * 1000),
-                title: title,
-                description: description,
-                status: status,
-                assignee:assignee,
-                duedate:duedate
-            }
-            setlistItems([...listItems, itemObj]);
-        }
+function ToDoPage() {
+  const [displayToDoItem, setDisplayToDoItem] = useState(false);
+  //Array of tasks
+  const [listItems, setlistItems] = useState([]);
+  // Current editing item
+  const [editingItem, setEditingItem] = useState({});
+  // Edit state
+  const [isEditing, setIsEditing] = useState(false);
+  //Flag to hold if the add new item button has to be shown
+  const [showAddNewItemButton, setshowAddNewItemButton] = useState(true);
+  //Function called from the child object to add a new item to the litsitems array
+  const addItemtoList = (title, description, status, assignee, duedate) => {
+    setDisplayToDoItem(false);
+    //Display the add button again when a new item is added
+    setshowAddNewItemButton(!showAddNewItemButton);
+    if (title !== "") {
+      let itemObj = {
+        itemID: Math.floor(Math.random() * 1000),
+        title: title,
+        description: description,
+        status: status,
+        assignee: assignee,
+        duedate: duedate,
+      };
+      setlistItems([...listItems, itemObj]);
     }
-    //Remove deleted item from the listitems array
-    const deleteItemFromList = (index)=> {
-        listItems.splice(index, 1);
-        setlistItems([...listItems]);
-    }
+  };
 
-    //Sort listitems array based on status
-    const sortItems = (sortdirection)=>{
-        if(sortdirection === 'asc'){
-            listItems.sort((a,b)=>a.status.localeCompare(b.status));
-        }
-        if(sortdirection === 'desc'){
-            listItems.sort((a,b)=>b.status.localeCompare(a.status));
-        }
-        setlistItems([...listItems]);
-    }
+  const udpateItemList = (
+    itemID,
+    title,
+    description,
+    status,
+    assignee,
+    duedate
+  ) => {
+    const newItemList = listItems.map((item) => {
+      if (item.itemID === itemID) {
+        item.title = title;
+        item.description = description;
+        item.status = status;
+        item.assignee = assignee;
+        item.duedate = duedate;
+      }
+      return item;
+    });
+    setlistItems(newItemList);
+  };
 
-    //Function that is called when the Add new item button is clicked
-    const showToDoItem = (e)=>{
-        //Displays the form to add a new item
-        setDisplayToDoItem(true);
-        //Hides add new item button
-        setshowAddNewItemButton(!showAddNewItemButton);
+  //Remove deleted item from the listitems array
+  const deleteItemFromList = (index) => {
+    listItems.splice(index, 1);
+    setlistItems([...listItems]);
+  };
+
+  //Sort listitems array based on status
+  const sortItems = (sortdirection) => {
+    if (sortdirection === "asc") {
+      listItems.sort((a, b) => a.status.localeCompare(b.status));
     }
-    
-    return(
-        <div className="todoPage">
-            {/* Display add new item button if the showAddNewItemButton flag is true */}
-            {showAddNewItemButton===true && <button className="btnAddnewtask"  onClick={showToDoItem}>Add New Task</button>}
-            {/* Display the add new item component to add a new item if the flag displayToDoItem is true */}
-            {displayToDoItem===true && <ToDoItem addItemtoList = { addItemtoList }/>}
-            <ToDoList listItems = {listItems}
-             ondeleteItemFromList={deleteItemFromList}
-             onsortItems={sortItems}/>
-        </div>
-    )
+    if (sortdirection === "desc") {
+      listItems.sort((a, b) => b.status.localeCompare(a.status));
+    }
+    setlistItems([...listItems]);
+  };
+
+  //Function that is called when the Add new item button is clicked
+  const showToDoItem = (e) => {
+    setEditingItem({});
+    //Displays the form to add a new item
+    setDisplayToDoItem(true);
+    //Hides add new item button
+    setshowAddNewItemButton(!showAddNewItemButton);
+  };
+
+  return (
+    <div className="todoPage">
+      {/* Display add new item button if the showAddNewItemButton flag is true */}
+      {showAddNewItemButton === true && (
+        <button className="btnAddnewtask" onClick={showToDoItem}>
+          Add New Task
+        </button>
+      )}
+      {/* Display the add new item component to add a new item if the flag displayToDoItem is true */}
+      {displayToDoItem === true && (
+        <ToDoItem
+          editingItem={editingItem}
+          setEditingItem={setEditingItem}
+          addItemtoList={addItemtoList}
+          isEditing={isEditing}
+          udpateItemList={udpateItemList}
+          setDisplayToDoItem={setDisplayToDoItem}
+          setIsEditing={setIsEditing}
+        />
+      )}
+      <ToDoList
+        listItems={listItems}
+        ondeleteItemFromList={deleteItemFromList}
+        onsortItems={sortItems}
+        setDisplayToDoItem={setDisplayToDoItem}
+        setEditingItem={setEditingItem}
+        setIsEditing={setIsEditing}
+      />
+    </div>
+  );
 }
 export default ToDoPage;
